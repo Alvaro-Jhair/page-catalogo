85b7863a6b4fb7c04902a95d6ae4ded12acbecea
Alvaro-Jhair <115193777+Alvaro-Jhair@users.noreply.github.com>
2026-07-27 18:44:56 -0500

    catalog(demo4): eliminar catálogo desde el panel de administración

--- data/catalogs/demo4.ts
+++
@@ -1,5 +0,0 @@
-import { CatalogEntrySchema } from "../schema";
-import type { CatalogEntry } from "../schema";
-import data from "./demo4.json";
-
-export const catalogEntry: CatalogEntry = CatalogEntrySchema.parse(data);
